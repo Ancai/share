f9a00891748b728ac8958ee2539c35e73dd7c842
liangancai <liangancai@10.30.6.12>
2013-01-28 08:26:47 +0800

    CCP 项目中的代码  案例  和导出Excel

--- web/src/com/share/util/POIUtil.java
+++ web/src/com/share/util/POIUtil.java
@@ -8,9 +8,13 @@ import java.util.Map;
 import java.util.Map.Entry;
 
 import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
+import org.apache.poi.hssf.usermodel.HSSFFont;
+import org.apache.poi.hssf.usermodel.HSSFRichTextString;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hssf.util.HSSFColor;
 
 /**
  * 工具类：利用Apache开放组件POI，操纵文档
@@ -30,34 +34,100 @@ public final class POIUtil {
 	/**
 	 * 生成Excel表格
 	 * 
-	 * @param title 文件名称
-	 * @param data 包含多个sheet的数据
-	 * @return HSSFWorkbook
+	 * @param dataRow 包含多个sheet的数据
+	 * @param titleRow 标题行
+	 * @return HSSFWorkbook 工作薄
 	 */
-	public HSSFWorkbook excel(String title, Map<String, List<Object[]>> data) {
+	public static HSSFWorkbook excel(Map<String, List<Object[]>> dataRow, Map<String, String[]> titleRow) {
 		//创建一个空白的WorkBook
 		HSSFWorkbook workBook = new HSSFWorkbook();
 		HSSFSheet sheet = null;
 		HSSFRow row = null;
 		HSSFCell cell = null;
+		HSSFCellStyle style = getStyle(workBook);
+		HSSFCellStyle style2 = getStyle2(workBook);
 		int colLength = 0;
-		for (Entry<String, List<Object[]>> entry : data.entrySet()) {
+		for (Entry<String, String[]> entry : titleRow.entrySet()) {
 			//基于上面的WorkBook创建属于此WorkBook的Sheet，
 			sheet = workBook.createSheet(entry.getKey());
-			List<Object[]> sheetData = entry.getValue();
-			for (Object[] objs : sheetData) {
-				//创建属于上面Sheet的Row，参数0可以是0～65535之间的任何一个，
-				//注意，尽管参数是Int类型，但是Excel最多支持65536行
-				row = sheet.createRow(sheetData.size());
+			sheet.setDefaultColumnWidth(15);
+			List<Object[]> sheetData = dataRow.get(entry.getKey());
+			//创建属于上面Sheet的Row，参数0可以是0～65535之间的任何一个，
+			//注意，尽管参数是Int类型，但是Excel最多支持65536行
+			row = sheet.createRow(0); //标题行
+			String[] titleCells = titleRow.get(entry.getKey());
+			for (int i = 0; i < titleCells.length; i++) {
+				cell = row.createCell(i, HSSFCell.CELL_TYPE_STRING);
+				cell.setCellStyle(style); //设置样式
+				HSSFRichTextString richText = new HSSFRichTextString(titleCells[i]);
+				cell.setCellValue(richText);
+			}
+			for (int i = 0; i < sheetData.size(); i++) {
+				Object[] objs = sheetData.get(i);
+				row = sheet.createRow(i+1); //数据行
 				colLength = objs.length;
-				for (int i = 0; i < colLength; i++) {
-					cell = row.createCell(i, HSSFCell.CELL_TYPE_STRING);
-					cell.setCellValue(String.valueOf(objs[i]));
+				for (int j = 0; j < colLength; j++) {
+					cell = row.createCell(j, HSSFCell.CELL_TYPE_STRING);
+					cell.setCellStyle(style2); //设置样式
+				    HSSFRichTextString richText = new HSSFRichTextString(null == objs[j] ? "" : objs[j].toString());
+					cell.setCellValue(richText);
 				}
 			}
-			
 		}
 		
 		return workBook;
+	}
+	
+	/**
+	 * 设置EXCEL 单元格的样式
+	 * 
+	 * @param workBook EXCEL工作簿
+	 * @return HSSFCellStyle
+	 */
+	private static HSSFCellStyle getStyle(HSSFWorkbook workBook) {
+		HSSFCellStyle style = workBook.createCellStyle();
+		// 设置这些样式
+        style.setFillForegroundColor(HSSFColor.SKY_BLUE.index);
+        style.setFillPattern(HSSFCellStyle.SOLID_FOREGROUND);
+        style.setBorderBottom(HSSFCellStyle.BORDER_THIN);
+        style.setBorderLeft(HSSFCellStyle.BORDER_THIN);
+        style.setBorderRight(HSSFCellStyle.BORDER_THIN);
+        style.setBorderTop(HSSFCellStyle.BORDER_THIN);
+        style.setAlignment(HSSFCellStyle.ALIGN_CENTER);
+        // 生成一个字体
+        HSSFFont font = workBook.createFont();
+        font.setColor(HSSFColor.VIOLET.index);
+        font.setFontHeightInPoints((short) 12);
+        font.setBoldweight(HSSFFont.BOLDWEIGHT_BOLD);
+        // 把字体应用到当前的样式
+        style.setFont(font);
+        
+        return style;
+	}
+	
+	/**
+	 * 设置EXCEL 单元格的样式
+	 * 
+	 * @param workBook EXCEL工作簿
+	 * @return HSSFCellStyle
+	 */
+	private static HSSFCellStyle getStyle2(HSSFWorkbook workBook) {
+		HSSFCellStyle style2 = workBook.createCellStyle();
+		// 生成并设置另一个样式
+        style2.setFillForegroundColor(HSSFColor.LIGHT_YELLOW.index);
+        style2.setFillPattern(HSSFCellStyle.SOLID_FOREGROUND);
+        style2.setBorderBottom(HSSFCellStyle.BORDER_THIN);
+        style2.setBorderLeft(HSSFCellStyle.BORDER_THIN);
+        style2.setBorderRight(HSSFCellStyle.BORDER_THIN);
+        style2.setBorderTop(HSSFCellStyle.BORDER_THIN);
+        style2.setAlignment(HSSFCellStyle.ALIGN_CENTER);
+        style2.setVerticalAlignment(HSSFCellStyle.VERTICAL_CENTER);
+        // 生成另一个字体
+        HSSFFont font2 = workBook.createFont();
+        font2.setBoldweight(HSSFFont.BOLDWEIGHT_NORMAL);
+        // 把字体应用到当前的样式
+        style2.setFont(font2);
+        
+        return style2;
 	}
 }
--- web/src/com/share/util/WebUtil.java
+++ web/src/com/share/util/WebUtil.java
@@ -8,6 +8,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
 import javax.servlet.ServletContext;
@@ -15,6 +16,12 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
+import net.sf.json.JSONArray;
+import net.sf.json.JsonConfig;
+import net.sf.json.util.CycleDetectionStrategy;
 
 
 
@@ -220,4 +227,136 @@ public final class WebUtil {
 		}
 		return null;
 	}
+	
+	/**
+	 * 转换JSON格式的数据
+	 * 		{id: 1, name: "tom"}
+	 * 
+	 * @param data 转换对象
+	 * @return String JSON格式的对象
+	 */
+	public static String jsonObj(Object data, JsonConfig jsonConfig) {
+		//JsonConfig jsonConfig = new JsonConfig();
+		// 解决自包含问题
+		jsonConfig.setCycleDetectionStrategy(CycleDetectionStrategy.LENIENT);
+		String tempData = JSONArray.fromObject(data,jsonConfig).toString();
+		return tempData.substring(1, tempData.length()-1);
+	}
+	
+	/**
+	 * 转换JSON格式的数据
+	 * 		[{country: "USA",visits: 4025,color: "#FF0F00" },
+	 *       {country: "China",visits: 1882,color: "#FF6600"},
+	 *       ......
+	 *      ]
+	 * 
+	 * @param data 转换对象
+	 * @return String JSON格式的数组
+	 */
+	public static String jsonArray(Object data) {
+		JsonConfig jsonConfig = new JsonConfig();  
+	    jsonConfig.setCycleDetectionStrategy(CycleDetectionStrategy.LENIENT);
+		return JSONArray.fromObject(data).toString();
+	}
+	
+	/**
+	 * 报表：Excel
+	 * 
+	 * @param reportName 导出Excel报表文件名称
+	 * @param htmlTable html形式的表格数据
+	 * @param response 响应对象
+	 */
+	public static void reportExcel(String reportName, String htmlTable, HttpServletResponse response) {
+		try {
+			//设置响应报头
+			response.reset();
+			response.addHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(reportName, ENCODING) + ".xls");
+			response.addHeader("Content-Length", "" + htmlTable.length());
+			response.setHeader("Pragma","No-cache"); 
+			response.setHeader("Cache-Control","no-cache"); 
+			response.setDateHeader("Expires", 0);
+			response.setContentType(MIME_TYPE_XLS);
+			response.setCharacterEncoding(ENCODING);
+			
+			//写入响应流数据
+			OutputStream os = new BufferedOutputStream(response.getOutputStream());
+			os.write(htmlTable.getBytes());
+			os.flush();
+			os.close();
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * 报表：Excel
+	 * 
+	 * @param reportName 导出Excel报表文件名称
+	 * @param workbook POI工作簿
+	 * @param response 响应对象
+	 */
+	public static void reportExcel(String reportName, HSSFWorkbook workbook, HttpServletResponse response) {
+		try {
+			//设置响应报头
+			response.reset();
+			response.addHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(reportName, ENCODING) + ".xls");
+			//response.addHeader("Content-Length", "" + workbook.getBytes().length);
+			response.setHeader("Pragma","No-cache"); 
+			response.setHeader("Cache-Control","no-cache"); 
+			response.setDateHeader("Expires", 0);
+			response.setContentType(MIME_TYPE_XLS);
+			response.setCharacterEncoding(ENCODING);
+			
+			//写入响应流数据
+			OutputStream os = new BufferedOutputStream(response.getOutputStream());
+			workbook.write(os);
+			os.flush();
+			os.close();
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * 报表：Word
+	 * 
+	 * @param reportName 报表文件名称
+	 * @param htmlTable html形式的表格数据
+	 * @param response 响应对象
+	 */
+	public static void reportWord(String reportName, String htmlTable, HttpServletResponse response) {
+		try {
+			//设置响应报头
+			response.reset();
+			response.addHeader("Content-Disposition", "attachment;filename=" + URLEncoder.encode(reportName, ENCODING) + ".doc");
+			response.addHeader("Content-Length", "" + htmlTable.length());
+			response.setHeader("Pragma","No-cache"); 
+			response.setHeader("Cache-Control","no-cache"); 
+			response.setDateHeader("Expires", 0);
+			response.setContentType(MIME_TYPE_DOC);
+			response.setCharacterEncoding(ENCODING);
+			
+			//写入响应流数据
+			OutputStream os = new BufferedOutputStream(response.getOutputStream());
+			os.write(htmlTable.getBytes());
+			os.flush();
+			os.close();
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
 }
